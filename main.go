a881e63e57232116c703cce5800a9678cfa81706
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-02-13 14:45:56 +0100

    Auto-commit: 2026-02-13 14:45:56

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import "net/http"
 
-func main 
+
+func main (){
+	http.HandleFunc("/", servePage)
+}
+
+func servePage (w http.Response, r *http.Request)  {
+	pages := map[string]string{
+		"/" : "home_page.html"
+	}
+
+	page := pages[r.URL.Path]
+	if page == ""{
+		page = 
+	}
+}
